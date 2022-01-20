ae5240d40fc82df97d436d30583dcdca8034cf81
Hector Soares <hector.soares@dtidigital.com.br>
2022-01-20 17:35:59 -0300

    create menu bar

--- src/components/organisms/menu-bar/index.tsx
+++ src/components/organisms/menu-bar/index.tsx
@@ -1,43 +1,125 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import { AppBar,  Box,  IconButton,  Toolbar } from '@mui/material';
-//import { makeStyles } from '@material-ui/core';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import HomeIcon from '@mui/icons-material/Home';
+import PaidIcon from '@mui/icons-material/Paid';
 
-//const drawerWidth = 0;
+const drawerWidth = 240;
 
-// const useStyles = makeStyles((theme: any) => {
-//     return {
-//         appbar: {
-//             width: `calc(100% - ${drawerWidth}px)`
-//         },
-//         toolbar: theme.mixins.toolbar,           
-//     }
-// });
+interface Props {
+  window?: () => Window;
+}
 
+export default function MenuBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-export default function MenuBar() {
+  const handleDrawerToggle = () => {
+    console.log(mobileOpen);
+    setMobileOpen(!mobileOpen);
+  };
 
-    // const classes = useStyles();
+  console.log(mobileOpen);
+
+  const drawer = (
+    <>
+      <Toolbar />
+      <Divider />
+      <List>
+        {['Inicio', 'Competição', 'Apostar'].map((text, index) => {
+          return (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {text == 'Inicio' && <HomeIcon />}
+                {text == 'Competição' && <BarChartIcon />}
+                {text == 'Apostar' && <PaidIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          );
+        })}
+      </List>
+      </>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" noWrap component="div">
             BBBet do Chupa Bola
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, 
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="temporary"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+      </Box>
     </Box>
   );
 }
